0e227e2933700a5bebc1b9a1c11f9024e3f42d72
xKeCo <kevcollazos@gmail.com>
2025-06-16 10:56:02 -0500

    ✨ feat(home): added habitaciones page

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,12 @@
-import Navbar from '@/components/Navbar/Navbar'
-import Reservation from '../components/Reservation/Reservation'
+import Hero from '@/components/home/Hero'
+import Habitaciones from '@/components/home/Habitaciones'
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
-      <Navbar />
+    <div className="scrollbar-hide font-istok overflow-x-hidden scroll-smooth md:h-screen md:snap-y md:snap-mandatory md:overflow-y-scroll">
+      <Hero />
 
-      <div className="fixed inset-0 -z-10">
-        <video
-          src="/videos/home.mp4"
-          autoPlay
-          muted
-          loop
-          className="h-full w-full object-cover opacity-[0.47]"
-        />
-      </div>
-
-      <div className="relative flex min-h-[calc(100vh-109px)] flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-between gap-28">
-          <h1 className="font-playfair text-foreground text-center text-8xl tracking-wide select-none sm:text-nowrap lg:text-9xl 2xl:text-[170px]">
-            CALLE <span className="font-semibold italic">FLORA</span>
-          </h1>
-
-          <div className="font-playfair text-foreground mt-4 max-w-2xl text-center tracking-wide select-none">
-            <span className="block text-xl leading-snug font-light text-white/90 sm:text-2xl">
-              &quot;Las <i className="font-semibold">plantas</i> eran mi{' '}
-              <i className="font-semibold">primer amor</i>.<br />
-              En su silencio <i className="font-semibold">entendí todo.</i>
-              &quot;
-            </span>
-
-            <span className="font-quicksand mt-2 block text-sm sm:text-base">
-              — Diario de Lecumberri, Álvaro Mutis
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <Reservation />
-    </main>
+      <Habitaciones />
+    </div>
   )
 }
---
+++ components/home/Habitaciones.tsx
@@ -0,0 +1,48 @@
+import { ArrowDownRight } from 'lucide-react'
+import Navbar from '../Navbar/Navbar'
+import Link from 'next/link'
+
+export default function Habitaciones() {
+  return (
+    <main className="relative h-screen snap-start">
+      <Navbar />
+
+      <div className="absolute inset-0 -z-10">
+        <video
+          src="/videos/habitaciones.mp4"
+          autoPlay
+          muted
+          loop
+          className="h-full w-full object-cover opacity-[0.47]"
+        />
+      </div>
+
+      <div className="relative flex min-h-screen flex-col items-center justify-center">
+        <div className="relative mt-20 flex w-5/6 flex-col items-start justify-between gap-14">
+          <h1 className="font-playfair text-foreground text-8xl tracking-wide select-none sm:text-nowrap lg:text-9xl 2xl:text-[170px]">
+            ORQU<i>I</i> <br />
+            <i>DEA</i>
+          </h1>
+
+          <div className="font-playfair text-foreground relative max-w-2xl text-start tracking-wide select-none">
+            <span className="block text-xl leading-snug font-light text-white/90 sm:text-2xl">
+              “En esta <i className="font-semibold">habitación</i> <br />
+              el tiempo se posa suave, <br />
+              como <i className="font-semibold">orquídea en rama</i>, <br />
+              como voz que no se apura”.
+            </span>
+          </div>
+
+          <Link
+            href="/"
+            className="absolute right-0 bottom-0 z-20 flex size-16 items-center justify-center rounded-full bg-white shadow-lg transition hover:scale-105 active:scale-95"
+            style={{ boxShadow: '0 2px 8px rgba(0,0,0,0.18)' }}
+            aria-label="Siguiente"
+          >
+            <ArrowDownRight className="size-9 font-light text-black" />
+          </Link>
+        </div>
+      </div>
+    </main>
+  )
+}
