b5bca9ed93f035e536264498961ac852c17d58bc
Juan David García Rincón <juandagarciadev@gmail.com>
2025-06-16 20:11:36 -0500

    ✨ feat: Integrate react-photoswipe-gallery for image gallery in Habitaciones component and add room images

--- components/home/Habitaciones.tsx
+++ components/home/Habitaciones.tsx
@@ -1,7 +1,90 @@
+'use client'
 import { ArrowDownRight } from 'lucide-react'
 import Link from 'next/link'
+import { Gallery, Item } from 'react-photoswipe-gallery'
+import 'photoswipe/dist/photoswipe.css'
 
 export default function Habitaciones() {
+  const dataSource = [
+    {
+      sourceId: 1, // needed to connect following data with Item component
+      original: '/images/room/1.jpg',
+      thumbnail: '/images/room/1.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 2, // needed to connect following data with Item component
+      original: '/images/room/2.jpg',
+      thumbnail: '/images/room/2.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 3, // needed to connect following data with Item component
+      original: '/images/room/3.jpg',
+      thumbnail: '/images/room/3.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 4, // needed to connect following data with Item component
+      original: '/images/room/4.jpg',
+      thumbnail: '/images/room/4.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 5, // needed to connect following data with Item component
+      original: '/images/room/5.jpg',
+      thumbnail: '/images/room/5.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 6, // needed to connect following data with Item component
+      original: '/images/room/6.jpg',
+      thumbnail: '/images/room/6.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 7, // needed to connect following data with Item component
+      original: '/images/room/7.jpg',
+      thumbnail: '/images/room/7.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 8, // needed to connect following data with Item component
+      original: '/images/room/8.jpg',
+      thumbnail: '/images/room/8.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 9, // needed to connect following data with Item component
+      original: '/images/room/9.jpg',
+      thumbnail: '/images/room/9.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 10, // needed to connect following data with Item component
+      original: '/images/room/10.jpg',
+      thumbnail: '/images/room/10.jpg',
+      width: '1450',
+      height: '966',
+    },
+    {
+      sourceId: 11, // needed to connect following data with Item component
+      original: '/images/room/11.jpg',
+      thumbnail: '/images/room/11.jpg',
+      width: '1450',
+      height: '966',
+    },
+  ]
+
   return (
     <main className="relative h-screen snap-start">
       <div className="absolute inset-0 -z-10">
@@ -28,6 +111,22 @@ export default function Habitaciones() {
               como <i className="font-semibold">orquídea en rama</i>, <br />
               como voz que no se apura”.
             </span>
+
+            <Gallery dataSource={dataSource}>
+              <Item
+                sourceId={1} // needed to connect Item component with data from dataSource
+              >
+                {({ ref, open }) => (
+                  <button
+                    ref={ref}
+                    onClick={open}
+                    className="mt-4 cursor-pointer rounded-md bg-white px-4 py-2 text-black"
+                  >
+                    Ver Galería
+                  </button>
+                )}
+              </Item>
+            </Gallery>
           </div>
 
           <Link
